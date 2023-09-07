b294364181ffddda950296f03f85462b7d1e2203
Miguel Senosiain <msenosiain@gmail.com>
2023-09-07 12:41:13 -0300

    Auth module (#1)
    Add /auth/register endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -12,11 +11,11 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(
       'mongodb+srv://gabriel:admin1234@cluster0.dycxonp.mongodb.net/',
     ),
-    ItemsModule,
-    UsersModule,
     AuthModule,
+    UsersModule,
+    ItemsModule,
   ],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginAuthDto } from './dto/login-auth.dto';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -13,25 +12,5 @@ export class AuthController {
   registerUser(@Body() userObject: RegisterAuthDto) {
     console.log({ body: userObject });
     return this.authService.register(userObject);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
   }
 }
